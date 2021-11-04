83324d15cc5d1d1dc328786619974e8f89b426f5
Jorge Mollericon <jlmollericon@gmail.com>
2021-11-04 19:59:18 -0400

    initial project, Operations base Class

---
+++ src/Operations.php
@@ -0,0 +1,22 @@
+<?php
+
+class Operations
+{
+  public function __constructor()
+  {
+    
+  }
+
+  public function sum($n1, $n2)
+  {
+    return $n1 + $n2;
+  }
+
+  public function div($n1, $n2)
+  {
+    return $n1 / $n2;
+  }
+
+}
+
+?>
